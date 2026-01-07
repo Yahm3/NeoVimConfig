6bbd9422353ff65c519b74658698ec674e5c1389
Yahm3 <240418541@tut4life.ac.za>
2026-01-07 20:43:53 +0200

    refactor: update neovim and an a new file for file icons

---
+++ lua/plugins/mini_icons.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvi-mini/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup()
+      -- This line tells Neovim to use mini.icons whenever a plugin
+      -- asks for nvim-web-devicons.
+      require("mini.icons").mock_nvim_web_devicons()
+    end,
+  },
+}
